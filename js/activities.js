59d48d410538d995601ea07442558ce789886746
Surabodee Phasuk-6609650707 <surabodee.pha@dome.tu.ac.th>
2023-12-10 02:19:04 +0700

    add dark light

--- js/activities.js
+++ js/activities.js
@@ -3,6 +3,39 @@ const config = {
 };
 const port = 8000;
 
+var root = document.querySelector(':root');
+var rs = getComputedStyle(root);
+
+  const lighttheme = document.getElementById("light");
+  const darktheme = document.getElementById("dark");
+  lighttheme.addEventListener("click", () => {
+    updateTheme();
+  });
+  darktheme.addEventListener("click", () => {
+    updateTheme();
+  });
+
+  function updateTheme() {
+    const lighttheme = document.getElementById("light");
+    if (lighttheme.checked) {
+      root.style.setProperty('--color-primary', 'black');
+      root.style.setProperty('--color-secondary', '#888');
+      root.style.setProperty('--color-accent', '#ccc');
+      root.style.setProperty('--color-background', 'white');
+      root.style.setProperty('--color-highlight', 'skyblue');
+      root.style.setProperty('--color-approved', 'green');
+      root.style.setProperty('--color-error', 'red');
+    } else {
+      root.style.setProperty('--color-primary', 'white');
+      root.style.setProperty('--color-secondary', '#999');
+      root.style.setProperty('--color-accent', '#444');
+      root.style.setProperty('--color-background', 'black');
+      root.style.setProperty('--color-highlight', '#e3f');
+      root.style.setProperty('--color-approved', 'darkpurple');
+      root.style.setProperty('--color-error', 'orange');
+    }
+  }
+
 // Function to validate Firstname and Lastname
 function validateName() {
   const fullnameInput = document.getElementById("fullname");
