9898b053fdb8b1916fae7f086c59d5051ec069e0
Suttipot <suttipot.pra@dome.tu.ac.th>
2023-12-10 04:39:57 +0700

    Change lang

--- js/activities.js
+++ js/activities.js
@@ -165,6 +165,120 @@ function validateFormOnInput() {
   validateLocation();
 }
 
+function changeLang(language) {
+
+  const labels = document.querySelectorAll("form label");
+  const smalls = document.querySelectorAll("form small");
+  const submitButton = document.querySelector("button[type='submit']");
+  for (const label of labels) {
+    const labelText = label.textContent;
+
+    if (language === "thai") {
+      if (labelText === "Firstname and Lastname") {
+        label.textContent = "ชื่อจริง และนามสกุล";
+      } else if (labelText === "Student ID") {
+        label.textContent = "รหัสประจำตัวนักศึกษา";
+      } else if (labelText === "University Email") {
+        label.textContent = "อีเมลมหาวิทยาลัย";
+      } else if (labelText === "Work/Activity Title") {
+        label.textContent = "กิจกรรมที่เข้าร่วม";
+      } else if (labelText === "Type of Work/Activity") {
+        label.textContent = "ประเภทของกิจกรรม";
+      } else if (labelText === "Academic Year") {
+        label.textContent = "ปีการศึกษา";
+      } else if (labelText === "Semester") {
+        label.textContent = "ภาคการศึกษา";
+      } else if (labelText === "Start Date/Time") {
+        label.textContent = "วันที่และเวลาเริ่มต้นกิจกรรม";
+      } else if (labelText === "End Date/Time") {
+        label.textContent = "วันที่และเวลาสิ้นสุดกิจกรรม";
+      } else if (labelText === "Location") {
+        label.textContent = "สถานที่จัดกิจกรรม";
+      } else if (labelText === "Description") {
+        label.textContent = "รายละเอียดกิจกรรม";
+      }
+    } else if (language === "english") {
+      if (labelText === "ชื่อจริง และนามสกุล") {
+        label.textContent = "Firstname and Lastname";
+      } else if (labelText === "รหัสประจำตัวนักศึกษา") {
+        label.textContent = "Student ID";
+      } else if (labelText === "อีเมลมหาวิทยาลัย") {
+        label.textContent = "University Email";
+      } else if (labelText === "กิจกรรมที่เข้าร่วม") {
+        label.textContent = "Work/Activity Title";
+      } else if (labelText === "ประเภทของกิจกรรม") {
+        label.textContent = "Type of Work/Activity";
+      } else if (labelText === "ปีการศึกษา") {
+        label.textContent = "Academic Year";
+      } else if (labelText === "ภาคการศึกษา") {
+        label.textContent = "Semester";
+      } else if (labelText === "วันที่และเวลาเริ่มต้นกิจกรรม") {
+        label.textContent = "Start Date/Time";
+      } else if (labelText === "วันที่และเวลาสิ้นสุดกิจกรรม") {
+        label.textContent = "End Date/Time";
+      } else if (labelText === "สถานที่จัดกิจกรรม") {
+        label.textContent = "Location";
+      } else if (labelText === "รายละเอียดกิจกรรม") {
+        label.textContent = "Description";
+      }
+    }
+  }
+  for (const small of smalls) {
+    const smallText = small.textContent;
+
+    if (language === "thai") {
+      if (smallText === "Only enter your first name and last name, middle name is not necessary.") {
+        small.textContent = "ใส่เฉพาะชื่อและนามสกุลเท่านั้น ไม่จำเป็นต้องใช้ชื่อกลาง";
+      } else if (smallText === "Please enter your 10-digit student ID.") {
+        small.textContent = "กรุณากรอกรหัสนักศึกษา 10 หลัก";
+      } else if (smallText === 'Please provide your university email in the format "xxx.yyy@dome.tu.ac.th".') {
+        small.textContent = 'กรุณาระบุอีเมลมหาวิทยาลัยของในรูปแบบ "xxx.yyy@dome.tu.ac.th"';
+      } else if (smallText === "Title must be a minimum of 4 characters.") {
+        small.textContent = "กิจกรรมที่เข้าร่วมต้องมีอย่างน้อย 4 ตัวอักษร";
+      } else if (smallText === "Please enter type of Work/Activity.") {
+        small.textContent = "กรุณาระบุประเภทกิจกรรม";
+      } else if (smallText === "Please enter Academic Year.") {
+        small.textContent = "กรุณาระบุปีการศึกษา.";
+      } else if (smallText === "Please enter semester.") {
+        small.textContent = "กรุณาระบุภาคการศึกษา";
+      } else if (smallText === "Please enter Start Date/Time.") {
+        small.textContent = "กรุณากรอกวันที่/เวลาเริ่มต้นกิจกรรม";
+      } else if (smallText === "Please enter End Date/Time.") {
+        small.textContent = "กรุณากรอกวันที่/เวลาสิ้นสุดกิจกรรม";
+      } else if (smallText === "Please enter your location.") {
+        small.textContent = "กรุณาระบุสถานที่จัดกิจกรรม";
+      }
+    } else if (language === "english") {
+      if (smallText === "ใส่เฉพาะชื่อและนามสกุลเท่านั้น ไม่จำเป็นต้องใช้ชื่อกลาง") {
+        small.textContent = "Only enter your first name and last name, middle name is not necessary.";
+      } else if (smallText === "กรุณากรอกรหัสนักศึกษา 10 หลัก") {
+        small.textContent = "Please enter your 10-digit student ID.";
+      } else if (smallText === 'กรุณาระบุอีเมลมหาวิทยาลัยของในรูปแบบ "xxx.yyy@dome.tu.ac.th"') {
+        small.textContent = 'Please provide your university email in the format "xxx.yyy@dome.tu.ac.th".';
+      } else if (smallText === "กิจกรรมที่เข้าร่วมต้องมีอย่างน้อย 4 ตัวอักษร") {
+        small.textContent = "Title must be a minimum of 4 characters.";
+      } else if (smallText === "กรุณาระบุประเภทกิจกรรม") {
+        small.textContent = "Please enter type of Work/Activity.";
+      } else if (smallText === "กรุณาระบุปีการศึกษา.") {
+        small.textContent = "Please enter Academic Year.";
+      } else if (smallText === "กรุณาระบุภาคการศึกษา") {
+        small.textContent = "Please enter semester.";
+      } else if (smallText === "กรุณากรอกวันที่/เวลาเริ่มต้นกิจกรรม") {
+        small.textContent = "Please enter Start Date/Time.";
+      } else if (smallText === "กรุณากรอกวันที่/เวลาสิ้นสุดกิจกรรม") {
+        small.textContent = "Please enter End Date/Time.";
+      } else if (smallText === "กรุณาระบุสถานที่จัดกิจกรรม") {
+        small.textContent = "Please enter your location.";
+    }
+  }
+  if (language === "thai") {
+    submitButton.textContent = "ยืนยัน";
+  } else if (language === "english") {
+    submitButton.textContent = "Submit";
+  }
+  }
+}
+
 // Function to fetch activity types from the backend
 async function fetchActivityTypes() {
   try {
